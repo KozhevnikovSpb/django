c2ef8d6c40a24f4b3df2a5de67225134f1320977
KozhevnikovSPB <57673312+KozhevnikovSpb@users.noreply.github.com>
2023-07-06 17:52:36 +0300

    Update views.py

--- postolit/web/views.py
+++ postolit/web/views.py
@@ -17,24 +17,6 @@ def index_2(request, id):
     print(id)
     return HttpResponse(f'Hello, World! {id}')
 
-
-def reg(request):
-    form = RegForm(request.POST or None) # None - это GET-запрос (просто загружаем страницу), request.POST - отправляем форму
-    message = None
-    if request.method == 'POST':
-        if form.is_valid():
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            print(email, password)
-            message = f'{email} {password} Вы успешно зарегистрировались'
-
-    context = {
-        'form': form,
-        'message': message
-    }
-    return render(request, template_name='reg.html', context=context)
-
-
 db = create_connection()
 
 
